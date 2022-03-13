cadf8d7412bcc96df2b11d0bd8e7887a320182db
caikan <caikanxp@gmail.com>
2022-03-13 23:38:28 +0800

    feat: bug repro.
    Error when both using <script setup> and <template lang="pug">

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const count = ref(0)
+const inc = () => count.value++
+</script>
+
+<template lang="pug">
+div
+  div count: {{ count }}
+  button(@click="inc")
+    | inc
+</template>
