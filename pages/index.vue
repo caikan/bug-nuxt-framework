d241ed38e276d1a891d6dd0637a15042a2907894
caikan <caikanxp@gmail.com>
2022-03-16 00:59:57 +0800

    repro: dev startup fatal

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
-<script setup>
-const count = ref(0)
-const inc = () => count.value++
-</script>
-
-<template lang="pug">
-div
-  div count: {{ count }}
-  button(@click="inc")
-    | inc
+<template>
+  <div>hello</div>
 </template>
